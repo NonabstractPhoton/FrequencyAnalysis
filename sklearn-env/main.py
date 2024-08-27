f561c82a3a081ffeea911f9daccd76b54730d481
NonabstractPhoton <46360592+NonabstractPhoton@users.noreply.github.com>
2024-08-27 19:56:12 -0400

    Added Sampling Weighting

--- sklearn-env/main.py
+++ sklearn-env/main.py
@@ -6,18 +6,18 @@ import os
 
 def main():
 
-    x = []
-    y = []
+    x, x_core = [], []
+    y, y_core = [], []
 
-    loadData(x,y,threshold=1e-4)
+    loadData(x,y,x_core,y_core)
 
-    singleTest(x,y,c=.0125)
-    # multiTest(x,y,cFunc=(lambda x: .0125 + .005*x), c_range=1,iterations=15)
-    # multiTest(x,y,cFunc=(lambda c: .0125+ .0125*c**2),c_range=5)
+    #singleTest(x,y,x_core,y_core,c=.125)
+    multiTest(x,y,x_core,y_core,cFunc=(lambda x: .05 + x * .0125),c_range=1,iterations=10)
 
 # '../training_set/data'
-def loadData(x,y,threshold):
-    for root, dirs, files in os.walk('../training_set/data'):
+def loadData(x,y,x_core,y_core,threshold=1e-4):
+    
+    for root, dirs, files in os.walk('../training_set/data/genres'):
         for name in files:
 
             vals = []
@@ -32,15 +32,42 @@ def loadData(x,y,threshold):
             x.append(vals)
 
 
-def singleTest(x,y,c):
+    for root, dirs, files in os.walk('../training_set/data/core'):
+        for name in files:
+
+            vals = []
+
+            with open(os.path.join(root,name)) as file:
+                line = file.readline().split(',')
+                y_core.append(int(line[1]))
+                for i in range(int(line[0])):
+                    val = float(file.readline().split(',')[1])
+                    vals.append(val if val > threshold else 0)
+
+            x_core.append(vals)
+
+
+def trainModel(x,y,x_core,y_core,c, core_weight=15):
 
     x_train, x_test, y_train, y_test = train_test_split(
-                x, y, test_size=0.1)                                            
+                x, y, test_size=0.05)       
+
+    weights = [1] * len(x_train) + [core_weight] * len(x_core)
+
+    x_train+=x_core
+    y_train+=y_core                                     
 
     clf = make_pipeline(StandardScaler(),
                         LinearSVC(tol=1e-5, C=c, penalty='l2', multi_class='ovr'))
 
-    clf.fit(x_train,y_train)
+    clf.fit(x_train,y_train, linearsvc__sample_weight=weights)
+
+    return clf, x_train, y_train, x_test, y_test
+
+
+def singleTest(x,y,x_core,y_core,c):
+
+    clf, x_train, y_train, x_test, y_test = trainModel(x,y,x_core,y_core,c)
 
     print("Expected:")
     print(y_test)
@@ -52,24 +79,17 @@ def singleTest(x,y,c):
     print(clf.score(x_train,y_train))
 
 
-def multiTest(x,y,cFunc,c_range:int,iterations:int=5):
+def multiTest(x,y,x_core,y_core,cFunc,c_range:int,iterations:int=5):
+    
     cmap = [] 
 
-    for c in range(c_range):
+    for c_kernel in range(c_range):
         avg_accuracy = 0
         avg_training_accuracy = 0
 
         for i in range(iterations):
 
-            x_train, x_test, y_train, y_test = train_test_split(
-                x, y, test_size=0.05)
-                                                
-
-            clf = make_pipeline(StandardScaler(),
-                                LinearSVC(tol=1e-5, C=cFunc(c), penalty='l2'))
-
-
-            clf.fit(x_train,y_train)
+            clf, x_train,y_train,x_test,y_test = trainModel(x,y,x_core,y_core,cFunc(c_kernel))
 
             avg_accuracy += clf.score(x_test,y_test)
             avg_training_accuracy += clf.score(x_train,y_train)
@@ -77,7 +97,7 @@ def multiTest(x,y,cFunc,c_range:int,iterations:int=5):
         avg_accuracy/=iterations
         avg_training_accuracy/=iterations        
 
-        cmap.append([cFunc(c),avg_accuracy,avg_training_accuracy])
+        cmap.append([cFunc(c_kernel),avg_accuracy,avg_training_accuracy])
 
     for row in cmap:
         print(row)
